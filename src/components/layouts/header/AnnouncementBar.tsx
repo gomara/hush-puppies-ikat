85e55766c84cace03884a03d02650cd6dd018474
Cristobal <cristobal@kimche.cl>
2024-01-18 10:17:34 +0000

    feat: add announcementbar and deliver offer com

---
+++ src/components/layouts/header/AnnouncementBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+async function AnnouncementBar() {
+  return (
+    <section className="bg-primary p-4 text-center text-lg font-bold text-primary-foreground sm:text-xl">
+      HOT SALE -30% EN SANDALIAS
+    </section>
+  );
+}
+
+export default AnnouncementBar;
