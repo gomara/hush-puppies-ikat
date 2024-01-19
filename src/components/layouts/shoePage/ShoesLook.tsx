3469728a2a9006f51fda1ae442a7a2f3d94c464b
Cristobal <cristobal@kimche.cl>
2024-01-19 11:29:18 +0000

    feat: add shoes recommendation and look section

---
+++ src/components/layouts/shoePage/ShoesLook.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ShoesLook() {
+  return (
+    <h3 className="text-md bg-footer p-2 font-serif font-semibold opacity-80">COMPLETA TU LOOK</h3>
+  );
+}
+
+export default ShoesLook;
