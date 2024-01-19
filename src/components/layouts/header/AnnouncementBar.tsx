995b7d4107253ef7e1c1eaa802bc3f0e78ddfd49
Cristobal <cristobal@kimche.cl>
2024-01-19 11:36:04 +0000

    fix: fix header styles

--- src/components/layouts/header/AnnouncementBar.tsx
+++ src/components/layouts/header/AnnouncementBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-async function AnnouncementBar() {
+function AnnouncementBar() {
   return (
     <section className="bg-primary p-4 text-center text-lg font-bold text-primary-foreground sm:text-xl">
       HOT SALE -30% EN SANDALIAS
--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -1,9 +1,6 @@
-'use client';
 import React from 'react';
 import Image from 'next/image';
 import { ChevronRight, Menu, ShoppingBag } from 'lucide-react';
-
-import useMobileDetect from '@/hooks/useMobileDetect';
 
 import { Input } from '../../ui/input';
 
@@ -11,34 +8,29 @@ import HeaderNav from './HeaderNav';
 import DeliveryOffer from './DeliveryOffer';
 
 function Header({ children }: { children?: React.ReactNode }) {
-  const isMobile = useMobileDetect();
-
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       {children}
-      <div className="relative flex flex-wrap items-baseline justify-between px-20 py-2">
-        {isMobile ? <Menu className="h-8 w-8 cursor-pointer" /> : null}
+      <div className="relative flex flex-wrap  items-baseline justify-between px-20 py-2">
+        <Menu className="h-8 w-8 cursor-pointer md:block lg:hidden" />
         <Image
           priority
           alt="Logo"
-          className="pt-4"
+          className="w-[150px] pt-4 md:w-[250px]"
           height={88}
           src="/hushpupies.png"
-          width={isMobile ? 150 : 250}
+          width={250}
         />
-        {!isMobile ? (
-          <div className="flex flex-col justify-between">
-            <div className="relative flex justify-end font-sans">
-              <ChevronRight className="absolute right-2 top-1/2 z-10 h-6 w-6 -translate-y-1/2 text-gray-500" />
-              <Input className="max-w-[300px] rounded-none" placeholder="BUSCAR" type="search" />
-            </div>
-            <DeliveryOffer />
+        <div className="hidden flex-col items-center justify-center md:items-end md:justify-between lg:flex">
+          <div className="relative flex max-w-[300px] font-sans md:w-[300px]">
+            <ChevronRight className="absolute right-2 top-1/2 z-10 h-6 w-6 -translate-y-1/2 text-gray-500" />
+            <Input className=" rounded-none" placeholder="BUSCAR" type="search" />
           </div>
-        ) : (
-          <ShoppingBag className="h-6 w-6 cursor-pointer" />
-        )}
+          <DeliveryOffer />
+        </div>
+        <ShoppingBag className="h-6 w-6 cursor-pointer md:block lg:hidden" />
       </div>
-      {!isMobile && <HeaderNav />}
+      <HeaderNav />
     </header>
   );
 }
