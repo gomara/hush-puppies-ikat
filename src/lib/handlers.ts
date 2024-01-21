ec03084fbf7b74f5b7102b6d85efe77c4b37c5cf
Cristobal <cristobal@kimche.cl>
2024-01-21 15:51:11 +0000

    feat: add handlers

--- src/lib/handlers.ts
+++ src/lib/handlers.ts
@@ -1,14 +1,40 @@
-import type Product from '@/interfaces/ProductInterface';
+import prisma from '@/lib/prisma';
 
-export async function fetchShoes(half?: boolean) {
-  const url = half
-    ? 'http://localhost:3000/api/products?half=true'
-    : 'http://localhost:3000/api/products?half=false';
-  const res = await fetch(url, {
-    method: 'GET',
-    next: { revalidate: 0 },
-  });
-  const data: Product[] = (await res.json()) as Product[];
+export async function getShoes(half?: boolean) {
+  try {
+    // This is just a simple example of how to use server components with route handlers.
+    // Because i only have a small amount of products, i'm just splitting them in half,
+    // to show them in two different carousels
+    let products;
 
-  return data;
+    if (half) {
+      products = await prisma.product.findMany({
+        take: Math.ceil((await prisma.product.count()) / 2),
+      });
+    } else {
+      const totalProducts = await prisma.product.count();
+
+      products = await prisma.product.findMany({
+        skip: Math.ceil(totalProducts / 2),
+        take: Math.floor(totalProducts / 2),
+      });
+    }
+
+    return products;
+  } catch (e) {
+    throw new Error('There was an error fetching the shoes');
+  }
+}
+
+export async function getShoe() {
+  // This is just for select a shoe if there is no shoe selected
+  // In a real app this would be a query for the selected shoe, not just the first one
+
+  try {
+    const product = await prisma.product.findFirst();
+
+    return product;
+  } catch (e) {
+    throw new Error('There was an error fetching the shoe');
+  }
 }
