b6982e3a77528317c7e42dc9bd2d3bddbbc689db
Cristobal <cristobal@kimche.cl>
2024-01-21 15:36:21 +0000

    fix: add post to upload shoes

---
+++ src/app/api/shoe/route.ts
@@ -0,0 +1,25 @@
+// import { NextResponse } from 'next/server';
+
+// import prisma from '@/lib/prisma';
+
+// This function was used to populate the database with the shoes
+// I'm not using it anymore, but i'm keeping it here just in case some one wants to use it lo populate their own database
+// for use it just uncomment and send a POST request to 'http://localhost:3000/api/shoe
+
+// export async function POST() {
+//   try {
+//     const formattedShoes = zapatos.map((shoe) => ({
+//       name: shoe.nombre,
+//       price: shoe.precio,
+//       imgURL: shoe.foto,
+//       ref: shoe.referencia,
+//       sizes: getShoeSizes(),
+//     }));
+
+//     await prisma.product.createMany({ data: formattedShoes });
+
+//     return NextResponse.json({ message: 'Created' }, { status: 200 });
+//   } catch (e) {
+//     return NextResponse.json({ message: 'Error' }, { status: 500 });
+//   }
+// }
