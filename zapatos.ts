7177886c1c4f8038f1c44d85efc3cfe13ffeb52a
Cristobal <cristobal@kimche.cl>
2024-01-21 15:35:08 +0000

    fix: add sizes to schema and fix config

---
+++ zapatos.ts
@@ -0,0 +1,125 @@
+// You cass use this file to upload the products to the database
+const zapatos = [
+  {
+    nombre: 'Zapato azul',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/pwpyw6C/1.jpg',
+  },
+  {
+    nombre: 'Zapato rojo',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/x5K1g43/2.jpg',
+  },
+  {
+    nombre: 'Zapato verde',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/6ZHYXw9/3.jpg',
+  },
+  {
+    nombre: 'Zapato gris',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/vZWM83J/4.jpg',
+  },
+  {
+    nombre: 'Zapato cafe',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/P5f655q/5.jpg',
+  },
+  {
+    nombre: 'Zapato miel',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/890wcvK/6.jpg',
+  },
+  {
+    nombre: 'Zapato aguamarina',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/mznrcLz/7.jpg',
+  },
+  {
+    nombre: 'Zapato piel',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/pxdM9qc/8.jpg',
+  },
+  {
+    nombre: 'Zapato perla',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/ZGpTWvc/9.jpg',
+  },
+  {
+    nombre: 'Zapato marinado',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/7kj7zLs/10.jpg',
+  },
+  {
+    nombre: 'Zapato azul1',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/qFMzHM0/11.jpg',
+  },
+  {
+    nombre: 'Zapato azul2',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/23bcj2F/15.jpg',
+  },
+  {
+    nombre: 'Zapato azul3',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/qFMzHM0/11.jpg',
+  },
+  {
+    nombre: 'Zapato azul4',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/h93jrFD/14.jpg',
+  },
+  {
+    nombre: 'Zapato azul5',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/23bcj2F/15.jpg',
+  },
+  {
+    nombre: 'Zapato azul6',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/zr5d54D/16.jpg',
+  },
+  {
+    nombre: 'Zapato azul7',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/fSwMdVK/17.jpg',
+  },
+  {
+    nombre: 'Zapato azul8',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/z5WnvPH/18.jpg',
+  },
+  {
+    nombre: 'Zapato azul9',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/DYKCSr5/19.jpg',
+  },
+  {
+    nombre: 'Zapato azul10',
+    precio: 5000,
+    referencia: 'G40',
+    foto: 'https://i.ibb.co/1qSddhd/20.jpg',
+  },
+];
+
+export default zapatos;
